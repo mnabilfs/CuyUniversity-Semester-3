6f66f441f61a2f82b3eca7244df5c702fff5b4a1
mnabilfs <mnabilfs22@gmail.com>
2025-06-07 19:35:04 +0700

    sesi-2

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-  res.send("Utama!");
+  res.send("Utama!!!");
 });
 
 app.get("/hello", (req, res) => {
+    console.log({urlParam: req.query});
   res.send("Hello World!");
 });
+
+app.post("/login", (req, res) => {
+    console.log({requestFromOutside: req.body});
+    res.send("login berhasil")
+})
+
+app.put("/username", (req, res) => {
+    console.log({updateData: req.body});
+    res.send("update berhasil!")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
